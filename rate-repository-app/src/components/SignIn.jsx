5b18a1f2a5cff6eb455f9598dd8505d914d966a9
fuka-innovations <fuad.kalhori@gmail.com>
2022-01-03 13:42:33 +0200

    10.9

--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -1,44 +1,59 @@
 import React from 'react';
 import { Button, StyleSheet, View } from 'react-native';
 import { Formik } from 'formik';
+import * as yup from 'yup';
 
 import Text from './Text';
 import FormikTextInput from './FormikTextInput';
-import theme from '../theme';
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create( {
   container: {
     //alignItems: 'center'
     padding: 10,
-    backgroundColor: 'white'
+    backgroundColor: 'white',
   },
   textInput: {
     borderWidth: 1,
     borderRadius: 3,
     marginVertical: 10,
     padding: 15,
-  }
+  },
 
-});
+} );
 
 const initialValues = {
   userName: '',
-  password: ''
-}
+  password: '',
+};
 
-const onSubmit = (values) => {
-  console.log(values)
-}
+const validationSchema = yup.object().shape( {
+  userName: yup.string().required( 'Username is required' ),
+  password: yup.string().required( 'Password is required' ),
+} );
+
+const submitHandler = ( values ) => {
+  console.log( values );
+};
+
+const SignInForm = ( { onSubmit } ) => {
+  return (
+      <View>
+        <FormikTextInput style={ styles.textInput } name={ 'userName' }
+                         placeholder={ 'Username' }/>
+        <FormikTextInput style={ styles.textInput } secureTextEntry
+                         name={ 'password' } placeholder={ 'Password' }/>
+        <Button title={ 'Sign in' } onPress={ onSubmit }/>
+      </View>
+  );
+};
 
 const SignIn = () => (
-    <View style={styles.container}>
-      <Text color={'primary'} fontWeight={'bold'}>The sign-in vjiew</Text>
-      <Formik initialValues={initialValues} onSubmit={onSubmit}>
-        <View>
-          <FormikTextInput style={styles.textInput} name={'userName'} placeholder={'Username'} />
-          <FormikTextInput style={styles.textInput} secureTextEntry name={'password'} placeholder={'Password'} />
-          <Button title={'Sign in'} onPress={onSubmit} />
-        </View>
+    <View style={ styles.container }>
+      <Text color={ 'text-primary' } fontWeight={ 'bold' }
+            style={ { fontSize: 20 } }>Sign in</Text>
+      <Formik initialValues={ initialValues } onSubmit={ submitHandler }
+              validationSchema={ validationSchema }>
+        { ( { handleSubmit } ) => <SignInForm onSubmit={ handleSubmit }/> }
       </Formik>
     </View>
 );
--- rate-repository-app/src/components/TextInput.jsx
+++ rate-repository-app/src/components/TextInput.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { TextInput as NativeTextInput, StyleSheet } from 'react-native';
+import theme from '../theme';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  error: {
+    borderWidth: 1,
+    borderRadius: 3,
+    marginVertical: 10,
+    padding: 15,
+    borderColor: theme.colors.error
+  }
+});
 
 const TextInput = ({ style, error, ...props }) => {
+
+  console.log('error: ', error);
+
+
   const textInputStyle = [style];
 
-  return <NativeTextInput style={textInputStyle} {...props} />;
+  return <NativeTextInput style= {!error ? textInputStyle : styles.error} {...props} />;
 };
 
 export default TextInput;
