5874d5511acfb854c7cb8177a851bf0c3ce97394
fuka-innovations <fuad.kalhori@gmail.com>
2022-01-05 12:38:15 +0200

    10.20

--- rate-repository-app/src/components/SingleRepository.jsx
+++ rate-repository-app/src/components/SingleRepository.jsx
@@ -1,18 +1,77 @@
-import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import React from 'react';
+import { View, StyleSheet, Text as NativeText, FlatList } from 'react-native';
 import Text from './Text';
 import { useQuery } from '@apollo/client';
-import { GET_REPOSITORY } from '../graphql/queries';
+import { GET_REPOSITORY, GET_REVIEWS } from '../graphql/queries';
 import { useParams } from 'react-router-native';
+import { format as formatDate } from 'date-fns';
+
 import RepositoryItem from './RepositoryItem';
+import theme from '../theme';
+
+const ItemSeparator = () => <View style={ styles.separator }/>;
+
+const ReviewItem = ( { node } ) => {
+  const date = new Date(node.createdAt)
+  const styles = StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      padding: 10,
+      backgroundColor: 'white'
+    },
+    rating: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: 30,
+      height: 30,
+      borderColor: theme.colors.primary,
+      borderWidth: 1,
+      borderRadius: 15
+    },
+    content: {
+      marginHorizontal: 15,
+
+    },
+    contentHeader: {
+      marginBottom: 10
+    }
+  });
+
+  return (
+      <View style={styles.container}>
+        <View style={styles.rating}>
+          <Text fontWeight={'bold'} color={'primary'}>{node.rating }</Text>
+        </View>
+        <View style={styles.content}>
+          <View style={styles.contentHeader}>
+            <Text fontWeight={'bold'}>{node.user.username }</Text>
+            <Text  color={'textSecondary'}>{formatDate(date, 'dd.MM.yyyy')}</Text>
+          </View>
+          <Text>{node.text }</Text>
+        </View>
+      </View>
+  );
+};
 
 const SingleRepository = () => {
   const { id } = useParams();
-  const { loading, error, data } = useQuery( GET_REPOSITORY,
+  const {
+    loading: loadingRepository,
+    error: errorRepository,
+    data: dataRepository,
+  } = useQuery( GET_REPOSITORY,
       { variables: { repositoryId: id } },
   );
 
-  if ( loading ) {
+  const {
+    error: errorReviews,
+    loading: loadingReviews,
+    data: dataReviews,
+  } = useQuery( GET_REVIEWS,
+      { variables: { repositoryId: id } },
+  );
+
+  if ( loadingRepository || loadingReviews ) {
     return (
         <View>
           <Text>Loading...</Text>
@@ -21,14 +80,25 @@ const SingleRepository = () => {
   }
 
   return (
-      <View>
-        <RepositoryItem { ...data.repository } />
-      </View>
+
+        <FlatList
+            data={ dataReviews.repository.reviews.edges }
+            keyExtractor={ item => item.node.id }
+            ItemSeparatorComponent={ ItemSeparator }
+            renderItem={ ( { item } ) => <ReviewItem { ...item } /> }
+            ListHeaderComponent={ () =>
+                <RepositoryItem { ...dataRepository.repository } /> }
+        />
+
 
   );
 };
 
 export default SingleRepository;
 
-const styles = StyleSheet.create( {} );
+const styles = StyleSheet.create( {
+  separator: {
+    height: 10,
+  },
+} );
 
--- rate-repository-app/src/graphql/queries.js
+++ rate-repository-app/src/graphql/queries.js
@@ -1,5 +1,29 @@
 import { gql } from '@apollo/client';
 
+export const GET_REVIEWS = gql`
+    query getReviews($repositoryId: ID!) {
+        repository(id: $repositoryId) {
+            id
+            reviews {
+                edges {
+                    node {
+                        id
+                        createdAt
+                        rating
+                        text
+                        user {
+                            id
+                            username
+                            reviewCount
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
+
+
 export const GET_REPOSITORIES = gql`
     query {
         repositories {
@@ -20,20 +44,19 @@ export const GET_REPOSITORIES = gql`
     }
 `;
 
-
 export const GET_REPOSITORY = gql`
     query getRepository($repositoryId: ID!) {
-            repository(id: $repositoryId) {
-                id
-                fullName
-                description
-                language
-                forksCount
-                stargazersCount
-                ratingAverage
-                reviewCount
-                ownerAvatarUrl
-                url
-            }
+        repository(id: $repositoryId) {
+            id
+            fullName
+            description
+            language
+            forksCount
+            stargazersCount
+            ratingAverage
+            reviewCount
+            ownerAvatarUrl
+            url
         }
+    }
 `;
